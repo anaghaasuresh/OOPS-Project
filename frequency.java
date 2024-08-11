f795f8cb1b72a9793e8faa0cd8c99f5bdcf1545f
anaghaasuresh <anaghakovilakam@gmail.com>
2024-08-11 18:09:26 +0530

    Add files via upload

---
+++ frequency.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class frequency
+{
+    public static void main(String[] args) {
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the string");
+            String str= sc.nextLine();
+            System.out.println("enter the character to find");
+            char ch=sc.next().charAt(0);
+            int freq=0;
+            for(int i=0;i<str.length();i++)
+            {
+                if(str.charAt(i)==ch)
+                {
+                    freq++;
+
+                }
+            }
+            System.err.println("the character" +ch+ "appears" +freq+ "times in the string");
+            sc.close();
+            
+                    }
+    }
+}
